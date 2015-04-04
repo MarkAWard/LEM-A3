4957b51cd8cb965197558c998237d3f9f0176489
Mark Ward <markmaw@gmail.com>
2015-04-04 17:56:26 -0400

    batch mode

--- TemporalLogExpPooling.lua
+++ TemporalLogExpPooling.lua
@@ -31,16 +31,32 @@ end
 
 function TemporalLogExpPooling:updateOutput(input)
 
-   local ndims = ((#input)[1] - self.kW) / self.dW + 1
+   local mode = #(#input)
+   if mode == 2 then
+      batch_size = 1
+      ndims = ((#input)[1] - self.kW) / self.dW + 1
+      cols = (#input)[2]
+      self.output = torch.zeros(ndims, cols)
+   elseif mode == 3 then
+      batch_size = (#input)[1]
+      ndims = ((#input)[2] - self.kW) / self.dW + 1
+      cols = (#input)[3]
+      self.output = torch.zeros(batch_size, ndims, cols)
+   end
 
-   self.output = torch.zeros(ndims, (#input)[2])
-
-   for j = 1, (#input)[2] do
-      idx = 1
-      for i = 1, ndims do
-         self.output[{i, j}] = torch.log( torch.sum( torch.exp(input[{{idx, idx + self.kW-1}, j}] * self.beta )) / self.kW ) / self.beta
-         idx = idx + self.dW
-      end    
+   local idx = 1
+   for k = 1, batch_size do
+      for j = 1, cols do
+         idx = 1
+         for i = 1, ndims do
+            if mode == 2 then
+               self.output[{i, j}] = torch.log( torch.sum( torch.exp(input[{{idx, idx + self.kW-1}, j}] * self.beta )) / self.kW ) / self.beta
+            else
+               self.output[k][{i, j}] = torch.log( torch.sum( torch.exp(input[k][{{idx, idx + self.kW-1}, j}] * self.beta )) / self.kW ) / self.beta
+            end
+            idx = idx + self.dW
+         end    
+      end
    end
 
    return self.output
@@ -48,15 +64,37 @@ end
 
 function TemporalLogExpPooling:updateGradInput(input, gradOutput)
 
+   local mode = #(#gradOutput)
+   if mode == 2 then
+      batch_size = 1
+      ndims = (#gradOutput)[1]
+      cols = (#gradOutput)[2]
+   elseif mode == 3 then
+      batch_size = (#gradOutput)[1]
+      ndims = (#gradOutput)[2]
+      cols = (#gradOutput)[3]
+   end
    self.gradInput = torch.zeros(input:size())
 
-   for col = 1, (#gradOutput)[2] do
-      for i = 1, (#gradOutput)[1] do
-         local denom = torch.sum( torch.exp( input[ {{i, i + self.kW - 1}, col}] * self.beta ) )
-         j_start = self.dW * (i - 1) + 1
-         for j = j_start, j_start + self.kW-1 do
-            num = torch.exp(  input[{j, col}] * self.beta )
-            self.gradInput[{j, col}] = self.gradInput[{j, col}] + ( gradOutput[{i, col}] * (num / denom) )
+   local denom = 1
+   for k = 1, batch_size do
+      for col = 1, cols do
+         for i = 1, ndims do
+            if mode == 2 then
+               denom = torch.sum( torch.exp( input[ {{i, i + self.kW - 1}, col}] * self.beta ) )
+            else
+               denom = torch.sum( torch.exp( input[k][ {{i, i + self.kW - 1}, col}] * self.beta ) )
+            end
+            j_start = self.dW * (i - 1) + 1
+            for j = j_start, j_start + self.kW-1 do
+               if mode == 2 then
+                  num = torch.exp(  input[{j, col}] * self.beta )
+                  self.gradInput[{j, col}] = self.gradInput[{j, col}] + ( gradOutput[{i, col}] * (num / denom) )
+               else
+                  num = torch.exp(  input[k][{j, col}] * self.beta )
+                  self.gradInput[k][{j, col}] = self.gradInput[k][{j, col}] + ( gradOutput[k][{i, col}] * (num / denom) )
+               end
+            end
          end
       end
    end
@@ -73,28 +111,28 @@ function TemporalLogExpPooling:empty()
    self.indices:storage():resize(0)
 end
 
-mod = nn.TemporalLogExpPooling(3,1,2)
+-- mod = nn.TemporalLogExpPooling(3,1,2)
 
-input_10_3 = torch.randn(10,3)
-input_4_5 = torch.randn(4,5)
-out_10_3 = mod:forward(input_10_3)
-out_4_5 = mod:forward(input_4_5)
+-- input_10_3 = torch.randn(10,3)
+-- input_4_5 = torch.randn(4,5)
+-- out_10_3 = mod:forward(input_10_3)
+-- out_4_5 = mod:forward(input_4_5)
 
-df1 = torch.randn(8,1)
-df2 = torch.randn(8,3)
-input = torch.randn(10,1)
-di1 = mod:backward(input, df1)
-di2 = mod:backward(input_10_3, df2)
+-- df1 = torch.randn(8,1)
+-- df2 = torch.randn(8,3)
+-- input = torch.randn(10,1)
+-- di1 = mod:backward(input, df1)
+-- di2 = mod:backward(input_10_3, df2)
 
 
-mod2 = nn.TemporalLogExpPooling(3,2,2)
+-- mod2 = nn.TemporalLogExpPooling(3,2,2)
 
-input_7_3 = torch.randn(7,3)
-input_9_5 = torch.randn(9,5)
-out_7_3 = mod2:forward(input_7_3)
-out_9_5 = mod2:forward(input_9_5)
+-- input_7_3 = torch.randn(7,3)
+-- input_9_5 = torch.randn(9,5)
+-- out_7_3 = mod2:forward(input_7_3)
+-- out_9_5 = mod2:forward(input_9_5)
 
-df11 = torch.randn(3,3)
-df22 = torch.randn(4,5)
-di11 = mod2:backward(input_7_3, df11)
-di22 = mod2:backward(input_9_5, df22)
+-- df11 = torch.randn(3,3)
+-- df22 = torch.randn(4,5)
+-- di11 = mod2:backward(input_7_3, df11)
+-- di22 = mod2:backward(input_9_5, df22)
