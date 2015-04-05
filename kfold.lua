a280e04b81c446012ee834932f0882b7cecbe68b
Mark Ward <markmaw@gmail.com>
2015-04-05 17:38:51 -0400

    rewrote to be disjoint partitions of the set

--- kfold.lua
+++ kfold.lua
@@ -1,22 +1,13 @@
 function folder(n,k)
-	local range=torch.range(1,n)
-	local giveme=n/k
-	local p=torch.ones(n)/n
-	local folds=torch.zeros(giveme,k)
+	folds = {}
+	local range = torch.randperm(n)
+	local giveme = n / k
 
-	for j=1,k do
-		local pointers=torch.multinomial(p,giveme,false)
-		
-		local temp1=range:index(1,pointers)
-		folds[{{},j}]=temp1
-		local temp2=torch.ones(#range)
-		temp2:indexFill(1,pointers,0)
-		temp2=temp2:byte()
-		range = range:maskedSelect(temp2)
-		p=p:maskedSelect(temp2)
+	for i = 1, k do
+		folds[i] = range[ {{ (i-1) * giveme + 1, i * giveme }} ]
 	end
-	
-return folds
+
+	return folds
 end
 
 
