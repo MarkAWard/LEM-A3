2bbe27231dc575b03837fe26565f81b12fa13289
elad663 <elad.zippory@gmail.com>
2015-04-11 01:16:03 -0400

    now running

--- 3_K80_training.lua
+++ 3_K80_training.lua
@@ -14,7 +14,6 @@ end
 
 print("loading data")
 all_tr_data=torch.load(opt.bufferPath)
-all_tr_data.folds=torch.load('data/folds.t7b')
 print(all_tr_data)
 
 
@@ -31,53 +30,26 @@ for i=1, shuffle:size(1) do
 	all_tr_data.y[i]=all_tr_data.y[shuffle[i]]
 end
 
-
-VL.x=all_tr_data.x:index(1,all_tr_data.folds[{{},k}])
-
-
-	VL.x=all_tr_data.x:index(1,all_tr_data.folds[{{},k}])
-	VL.y=all_tr_data.y:index(1,all_tr_data.folds[{{},k}])
-
-	--vl_pointers=all_tr_data.folds[{{},k}]:long()	
-
-	--for i=1, vl_pointers:size(1) do
-	--	VL.x[{i,{},{}}]=all_tr_data.x[{vl_pointers[i],{},{}}]
-	--	VL.y[i]=all_tr_data.y[vl_pointers[i]]
-	--end
+temp=.8*(all_tr_data.x:size(1))
+VL.x=all_tr_data.x[{{temp+1,(all_tr_data.x:size(1))},{},{}}]
+VL.y=all_tr_data.y[{{temp+1,(all_tr_data.x:size(1))}}]
 
 
-	pointers=torch.ones(#all_tr_data.folds)
-	pointers:indexFill(2,torch.LongTensor({k}),0)
-	
-	tr_pointers=all_tr_data.folds:maskedSelect(pointers:byte()):long()
+TR.x=all_tr_data.x[{{1,temp},{},{}}]
+TR.y=all_tr_data.y[{{1,temp}}]
 
 
-	TR.x=torch.zeros(585000,30,200)
+print('==> calling train_model')
 
--- this crushes memory
---	TR.x=all_tr_data.x:index(1,tr_pointers)
+if opt.type == 'cuda' then
+	model=model:cuda()
+	criterion=criterion:cuda()
+end	
 
--- This should work. doing a loop instead.
---	TR.x:indexCopy(1,tr_pointers,all_tr_data.x)
-	
+--print('==> loading previous model')
+--model=torch.load('training/model_smallerboy_200_run_trying_fold_1_epoch_4.net' )
 
-	for i=1, tr_pointers:size(1) do
-		TR.x[{i,{},{}}]=all_tr_data.x[{tr_pointers[i],{},{}}]
-	end
-
-	TR.y=all_tr_data.y:index(1,tr_pointers)
-
-	print('==> calling train_model')
-
-	if opt.type == 'cuda' then
-		model=model:cuda()
-		criterion=criterion:cuda()
-	end	
-
-	print('==> loading previous model')
-	--model=torch.load('training/model_smallerboy_200_run_trying_fold_1_epoch_4.net' )
-
-	train_model(model, criterion, TR.x, TR.y, VL.x, VL.y, opt)
+train_model(model, criterion, TR.x, TR.y, VL.x, VL.y, opt)
 
 
 
