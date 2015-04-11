a142eb8639ae88c994183bba83d2a1fe3c684e09
Mark Ward <markmaw@gmail.com>
2015-04-11 18:50:19 -0400

    modularize model selection

--- model.lua
+++ model.lua
@@ -1,210 +1,202 @@
-	
-if opt.model=='elad' then
-	
-    model = nn.Sequential()
-	model:add(nn.TemporalConvolution(200, 300, 4, 1))
-	model:add(nn.Tanh())
-	model:add(nn.TemporalMaxPooling(3, 1))
+-- model definition module
+require 'nn'	
 
-	model:add(nn.TemporalConvolution(300, 300, 5, 2))
-	model:add(nn.Tanh())
-	model:add(nn.TemporalMaxPooling(3, 1))
+local M = {}
 
-    model:add(nn.Reshape(300*44, true))	
-    model:add(nn.Linear(300*44, 5))
-	model:add(nn.LogSoftMax())
-	
-    criterion = nn.ClassNLLCriterion()
-	
+function M:select_model(opt, dict_size)
 
-end
+	if opt.model == 'lookup_elad' then 
 
+	    model = nn.Sequential()
+	    model:add(nn.LookupTable(dict_size, 200))
+		model:add(nn.TemporalConvolution(200, 300, 4, 1))
+		model:add(nn.Tanh())
+		model:add(nn.TemporalMaxPooling(3, 1))
 
-if opt.model=='bigboy' then
-	
-	--bigrams
-    bigrams = nn.Sequential()
-	bigrams:add(nn.TemporalConvolution(200, 250, 2, 1))
-	bigrams:add(nn.ReLU())
-	bigrams:add(nn.TemporalConvolution(250, 300, 2, 1))
-	bigrams:add(nn.ReLU())
-	bigrams:add(nn.TemporalMaxPooling(3, 1))
-	bigrams:add(nn.TemporalConvolution(300, 300, 2, 1))
-	bigrams:add(nn.ReLU())
-	bigrams:add(nn.TemporalMaxPooling(4, 1))
+		model:add(nn.TemporalConvolution(300, 300, 5, 2))
+		model:add(nn.Tanh())
+		model:add(nn.TemporalMaxPooling(3, 1))
 
-	
---	output=bigrams:forward(all_tr_data.x)
---	print(#output)
-	--10000
-    --22
-    --110
-	
-	
-	--trigrams
-    trigrams = nn.Sequential()
-	trigrams:add(nn.TemporalConvolution(200, 250, 3, 1))
-	trigrams:add(nn.ReLU())
-	trigrams:add(nn.TemporalConvolution(250, 300, 3, 1))
-	trigrams:add(nn.ReLU())
-	trigrams:add(nn.TemporalMaxPooling(3, 1))
-	trigrams:add(nn.TemporalConvolution(300, 300, 2, 1))
-	trigrams:add(nn.ReLU())
-	trigrams:add(nn.TemporalMaxPooling(2, 1))
-	
---	output=trigrams:forward(all_tr_data.x)
---	print(#output)
-	--10000
-    --22
-    --110
-
-	
-	
-	--quadgrams
-    quadgrams = nn.Sequential()
-	quadgrams:add(nn.TemporalConvolution(200, 250, 4, 1))
-	quadgrams:add(nn.ReLU())
-	quadgrams:add(nn.TemporalConvolution(250, 300, 4, 1))
-	quadgrams:add(nn.ReLU())
-	quadgrams:add(nn.TemporalMaxPooling(3, 1))
-	
---	output=quadgrams:forward(all_tr_data.x)
---	print(#output)
-	--10000
-    --22
-    --110
-	
-	
-	par=nn.Concat(2)
-	par:add(bigrams)
-	par:add(trigrams)
-	par:add(quadgrams)
-
-    model = nn.Sequential()
-	model:add(par)
-	
-	model:add(nn.TemporalConvolution(300, 300, 4, 2))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(3, 2))
-	
-	model:add(nn.TemporalConvolution(300, 300, 4, 3))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(3, 1))
-
-	model:add(nn.TemporalConvolution(300, 300, 2, 1))
-	model:add(nn.ReLU())
-	
-	model:add(nn.Reshape(300, true))	
-    model:add(nn.Linear(300, 5))
-	model:add(nn.LogSoftMax())
-	
-    criterion = nn.ClassNLLCriterion()
-end
-
-
-
-
-
-if opt.model=='smallerboy' then
-	
-	--bigrams
-    bigrams = nn.Sequential()
-	bigrams:add(nn.TemporalConvolution(50, 100, 2, 1))
-	bigrams:add(nn.ReLU())
-	bigrams:add(nn.TemporalMaxPooling(3, 2))
-	
+	    model:add(nn.Reshape(300*44, true))	
+	    model:add(nn.Linear(300*44, 5))
+		model:add(nn.LogSoftMax())
 		
-	--trigrams
-    trigrams = nn.Sequential()
-	trigrams:add(nn.TemporalConvolution(50, 100, 3, 1))
-	trigrams:add(nn.ReLU())
-	trigrams:add(nn.TemporalMaxPooling(2, 2))
-	
-	
-	
-	--quadgrams
-    quadgrams = nn.Sequential()
-	quadgrams:add(nn.TemporalConvolution(50, 100, 4, 1))
-	quadgrams:add(nn.ReLU())
-	quadgrams:add(nn.TemporalMaxPooling(1, 2))
-
-	
-	par=nn.Concat(2)
-	par:add(bigrams)
-	par:add(trigrams)
-	par:add(quadgrams)
-
-    model = nn.Sequential()
-	model:add(par)
-
-	
-	model:add(nn.TemporalConvolution(100, 150, 4, 2))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(4, 2))
-	
-
-
-	model:add(nn.TemporalConvolution(150, 170, 3, 3))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(3, 1))
-
-	
-	model:add(nn.Reshape(170, true))	
-    model:add(nn.Linear(170, 5))
-	model:add(nn.LogSoftMax())
-	
-    criterion = nn.ClassNLLCriterion()
-end
-
-
-if opt.model=='smallerboy_200' then
-	
-	--bigrams
-    bigrams = nn.Sequential()
-	bigrams:add(nn.TemporalConvolution(200, 300, 2, 1))
-	bigrams:add(nn.ReLU())
-	bigrams:add(nn.TemporalMaxPooling(3, 2))
-	
+	elseif opt.model=='elad' then
 		
-	--trigrams
-    trigrams = nn.Sequential()
-	trigrams:add(nn.TemporalConvolution(200, 300, 3, 1))
-	trigrams:add(nn.ReLU())
-	trigrams:add(nn.TemporalMaxPooling(2, 2))
-	
-	
-	
-	--quadgrams
-    quadgrams = nn.Sequential()
-	quadgrams:add(nn.TemporalConvolution(200, 300, 4, 1))
-	quadgrams:add(nn.ReLU())
-	quadgrams:add(nn.TemporalMaxPooling(1, 2))
+	    model = nn.Sequential()
+		model:add(nn.TemporalConvolution(200, 300, 4, 1))
+		model:add(nn.Tanh())
+		model:add(nn.TemporalMaxPooling(3, 1))
 
-	
-	par=nn.Concat(2)
-	par:add(bigrams)
-	par:add(trigrams)
-	par:add(quadgrams)
+		model:add(nn.TemporalConvolution(300, 300, 5, 2))
+		model:add(nn.Tanh())
+		model:add(nn.TemporalMaxPooling(3, 1))
 
-    model = nn.Sequential()
-	model:add(par)
-
-	
-	model:add(nn.TemporalConvolution(300, 400, 4, 2))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(4, 2))
-	
+	    model:add(nn.Reshape(300*44, true))	
+	    model:add(nn.Linear(300*44, 5))
+		model:add(nn.LogSoftMax())
 
 
-	model:add(nn.TemporalConvolution(400, 400, 3, 3))
-	model:add(nn.ReLU())
-	model:add(nn.TemporalMaxPooling(3, 1))
+	elseif opt.model=='bigboy' then
+		
+		--bigrams
+	    bigrams = nn.Sequential()
+		bigrams:add(nn.TemporalConvolution(200, 250, 2, 1))
+		bigrams:add(nn.ReLU())
+		bigrams:add(nn.TemporalConvolution(250, 300, 2, 1))
+		bigrams:add(nn.ReLU())
+		bigrams:add(nn.TemporalMaxPooling(3, 1))
+		bigrams:add(nn.TemporalConvolution(300, 300, 2, 1))
+		bigrams:add(nn.ReLU())
+		bigrams:add(nn.TemporalMaxPooling(4, 1))
 
+	--	output=bigrams:forward(all_tr_data.x)
+	--	print(#output)
+		--10000
+	    --22
+	    --110	
+		
+		--trigrams
+	    trigrams = nn.Sequential()
+		trigrams:add(nn.TemporalConvolution(200, 250, 3, 1))
+		trigrams:add(nn.ReLU())
+		trigrams:add(nn.TemporalConvolution(250, 300, 3, 1))
+		trigrams:add(nn.ReLU())
+		trigrams:add(nn.TemporalMaxPooling(3, 1))
+		trigrams:add(nn.TemporalConvolution(300, 300, 2, 1))
+		trigrams:add(nn.ReLU())
+		trigrams:add(nn.TemporalMaxPooling(2, 1))
+		
+	--	output=trigrams:forward(all_tr_data.x)
+	--	print(#output)
+		--10000
+	    --22
+	    --110
+
+		--quadgrams
+	    quadgrams = nn.Sequential()
+		quadgrams:add(nn.TemporalConvolution(200, 250, 4, 1))
+		quadgrams:add(nn.ReLU())
+		quadgrams:add(nn.TemporalConvolution(250, 300, 4, 1))
+		quadgrams:add(nn.ReLU())
+		quadgrams:add(nn.TemporalMaxPooling(3, 1))
+		
+	--	output=quadgrams:forward(all_tr_data.x)
+	--	print(#output)
+		--10000
+	    --22
+	    --110
+			
+		par=nn.Concat(2)
+		par:add(bigrams)
+		par:add(trigrams)
+		par:add(quadgrams)
+
+	    model = nn.Sequential()
+		model:add(par)
+		
+		model:add(nn.TemporalConvolution(300, 300, 4, 2))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(3, 2))
+		
+		model:add(nn.TemporalConvolution(300, 300, 4, 3))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(3, 1))
+
+		model:add(nn.TemporalConvolution(300, 300, 2, 1))
+		model:add(nn.ReLU())
+		
+		model:add(nn.Reshape(300, true))	
+	    model:add(nn.Linear(300, 5))
+		model:add(nn.LogSoftMax())
+		
+
+	elseif opt.model=='smallerboy' then
+		
+		--bigrams
+	    bigrams = nn.Sequential()
+		bigrams:add(nn.TemporalConvolution(50, 100, 2, 1))
+		bigrams:add(nn.ReLU())
+		bigrams:add(nn.TemporalMaxPooling(3, 2))
+			
+		--trigrams
+	    trigrams = nn.Sequential()
+		trigrams:add(nn.TemporalConvolution(50, 100, 3, 1))
+		trigrams:add(nn.ReLU())
+		trigrams:add(nn.TemporalMaxPooling(2, 2))
+		
+		--quadgrams
+	    quadgrams = nn.Sequential()
+		quadgrams:add(nn.TemporalConvolution(50, 100, 4, 1))
+		quadgrams:add(nn.ReLU())
+		quadgrams:add(nn.TemporalMaxPooling(1, 2))
+
+		par=nn.Concat(2)
+		par:add(bigrams)
+		par:add(trigrams)
+		par:add(quadgrams)
+
+	    model = nn.Sequential()
+		model:add(par)
+		
+		model:add(nn.TemporalConvolution(100, 150, 4, 2))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(4, 2))
+		
+		model:add(nn.TemporalConvolution(150, 170, 3, 3))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(3, 1))
+
+		model:add(nn.Reshape(170, true))	
+	    model:add(nn.Linear(170, 5))
+		model:add(nn.LogSoftMax())
+		
+	elseif opt.model=='smallerboy_200' then
+		
+		--bigrams
+	    bigrams = nn.Sequential()
+		bigrams:add(nn.TemporalConvolution(200, 300, 2, 1))
+		bigrams:add(nn.ReLU())
+		bigrams:add(nn.TemporalMaxPooling(3, 2))
+			
+		--trigrams
+	    trigrams = nn.Sequential()
+		trigrams:add(nn.TemporalConvolution(200, 300, 3, 1))
+		trigrams:add(nn.ReLU())
+		trigrams:add(nn.TemporalMaxPooling(2, 2))
+		
+		--quadgrams
+	    quadgrams = nn.Sequential()
+		quadgrams:add(nn.TemporalConvolution(200, 300, 4, 1))
+		quadgrams:add(nn.ReLU())
+		quadgrams:add(nn.TemporalMaxPooling(1, 2))
+
+		par=nn.Concat(2)
+		par:add(bigrams)
+		par:add(trigrams)
+		par:add(quadgrams)
+
+	    model = nn.Sequential()
+		model:add(par)
+
+		model:add(nn.TemporalConvolution(300, 400, 4, 2))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(4, 2))
+		
+		model:add(nn.TemporalConvolution(400, 400, 3, 3))
+		model:add(nn.ReLU())
+		model:add(nn.TemporalMaxPooling(3, 1))
+		
+		model:add(nn.Reshape(400, true))	
+	    model:add(nn.Linear(400, 5))
+		model:add(nn.LogSoftMax())
+		
+	else
+		error("Invalid model name")
+	end
 	
-	model:add(nn.Reshape(400, true))	
-    model:add(nn.Linear(400, 5))
-	model:add(nn.LogSoftMax())
-	
-    criterion = nn.ClassNLLCriterion()
+	return model, nn.ClassNLLCriterion()
+
 end
 
+return M
