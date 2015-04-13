69e2c9dbda38e8bffea049821cb9a1faa666cbb2
Lefteris Soulas <elnio@Lefteriss-MacBook-Air.local>
2015-04-13 07:26:37 -0400

    final additions

--- LEM_model.sh
+++ LEM_model.sh
@@ -11,10 +11,4 @@ module load cuda/6.5.12
 module load torch-deps/7
 
 cd $HOME
-
-th 0_K80_options.lua -mode train -gpudevice 1 -bufferPath_x data/TR_x_100x200.t7b -bufferPath_y data/TR_y_100x200.t7b >>job.log
-
-
-
-th 0_K80_options.lua -mode train -bufferPath_x data/TR_x_100x200.t7b -bufferPath_y data/TR_y_100x200.t7b -learningRate 0.05 -momentum 0.5 -learningRateDecay 0.001 -seed 11
-
+th submissionScript.lua -model model_elad_run_simple_epoch_9.net
