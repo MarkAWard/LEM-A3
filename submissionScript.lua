04a926cd031cf516e42f41e9fa557bc4e9a14eb3
Lefteris Soulas <elnio@Lefteriss-MacBook-Air.local>
2015-04-13 00:38:36 -0400

    init version of submissionScript

---
+++ submissionScript.lua
@@ -0,0 +1,36 @@
+require 'functions_processing_training'
+
+local wordvector_table = torch.load('glove.t7b')
+local model = torch.load( 'model_elad_run_simple_epoch_4.net' )
+model:evaluate()
+while true do
+    local review = io.read()
+    if not review then break end
+    -- opt.max_length is fixed to 100 and word embedding size is fixed to 200.
+    local data   = torch.zeros( 100, 200)
+    model:evaluate()
+    review = preprocess_text(review)
+    
+    doc_size = 1 
+    for word in review:gmatch("%S+") do
+    	if doc_size > 100 then  break end
+    	if wordvector_table[word] then
+    		data[{{ doc_size, {} }}]:add( wordvector_table[word][1] )
+            doc_size = doc_size + 1
+        else 
+        	if wordvector_table[word:gsub("%p+", "")] then
+        		data[{{ doc_size, {} }}]:add( wordvector_table[ word:gsub("%p+", "") ][1] )
+        		doc_size = doc_size + 1
+           end
+        end
+
+		local i=1
+        while doc_size <= 100 do
+			data[{{ doc_size, {} }}]=data[{{ i, {} }}]
+			i=i+1
+			doc_size=doc_size+1
+        end
+    end
+    print( model:forward(data) )
+	--io.write(  )  
+end
